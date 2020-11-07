84d4a1fba133d65cd0faf1fb6f2e7f04da8e6987
Alexander Khoroshikh <xad71rus@gmail.com>
2020-11-07 13:34:31 +0300

    feat: finished proof-of-concept of per-component rendering app

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
+import fastify from 'fastify';
 import { compile } from './core';
 
-compile('./test/App.svelte', { title: 'test' });
+const app = fastify({ logger: true });
+
+app.get('/test', async (req) => {
+  const { body, params } = req;
+
+  console.log('/test', { body, params });
+  return compile('./test/App.svelte', { title: 'test' });
+});
+
+app.get('/hero', async (req) => {
+  const { body, params } = req;
+
+  console.log('/hero', { body, params });
+  return compile('./test/Hero.svelte', { someProp: 'bla' });
+});
+
+const start = async () => {
+  try {
+    await app.listen(3000);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+start();
