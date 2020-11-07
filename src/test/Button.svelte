fef1d70cbf4c23bd5372a43d22dc63930300f3ba
Alexander Khoroshikh <xad71rus@gmail.com>
2020-11-07 13:34:01 +0300

    chore: added some more test components

---
+++ src/test/Button.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let isHighlighted = false;
+</script>
+
+<style>
+    .isHighlighted {
+        background-color: crimson;
+    }
+</style>
+
+<button type="button" class:isHighlighted>
+    <slot></slot>
+</button>
---
+++ src/test/Hero.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Button from './Button.svelte';
+    export let someProp = 'default';
+</script>
+
+<article>
+    <div>
+        {someProp}
+    </div>
+    <Button isHighlighted>{someProp}</Button>
+</article>
