4d8a4d6d2a2f6efa532a9ea0926422e5dbdfd702
Alexander Khoroshikh <xad71rus@gmail.com>
2020-11-07 12:56:53 +0300

    fix: removed all non-ssr-related code from compile helper

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,26 +1,17 @@
 import type { SvelteComponent } from 'svelte';
 import path from 'path';
-import fs from 'fs/promises';
+
+type Props = {
+  [key: string]: unknown;
+};
 
 require('svelte/register');
 
-const svelte = require('svelte/compiler');
-
-export const compile = async (pathToComp: string, props: any) => {
+export const compile = async (pathToComp: string, props: Props) => {
   const resolvedPath = path.resolve(__dirname, '../', pathToComp);
   const Comp: SvelteComponent = require(resolvedPath);
-  const compSource = await fs.readFile(resolvedPath, 'utf-8');
 
-  const { html, css } = Comp.default.render(props);
+  const { html } = Comp.default.render(props);
 
-  console.log(svelte);
-
-  const svelteCompiled = svelte.compile(compSource, {
-    generate: 'dom',
-    hydratable: true,
-  });
-
-  console.log(html, css);
-
-  console.log(svelteCompiled);
+  return html;
 };
